c3c6eee68de646bba11dfe47cf5a6be8a3cdb787
dhruv <dhruvrayat50@gmail.com>
2022-12-07 22:39:56 +1300

    Update: Production is for nerds

--- src/routes/images/capybara.ts
+++ src/routes/images/capybara.ts
@@ -5,9 +5,7 @@ let random = require("../../core/utilities/random"); // Utilities
 
 router.get("/", (req: Request, res: Response) => {
   let num: number = random.get_random_number(1, 7);
-  res.sendFile(
-    path.join(`${__dirname}/../../core/images/capybara/capy${num}.png`)
-  );
+  res.sendFile(path.join(`${__dirname}/capybara/capy${num}.png`));
 });
 
 module.exports = router;
